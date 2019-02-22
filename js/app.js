6b6f58ff95b6da9dda37be34cf3bbdac49da0d15
Lyndon Panton <LyndonPanton@gmail.com>
2019-02-22 13:13:57 +0000

    Added logic for wins via a row

--- js/app.js
+++ js/app.js
@@ -2,20 +2,14 @@ let year = (new Date()).getFullYear();
 let yearSpan = document.getElementById("year");
 yearSpan.textContent = year;
 
+let topRow = document.getElementById("row-top");
+let middleRow = document.getElementById("row-middle");
+let bottomRow = document.getElementById("row-bottom");
+
 let turn = "X";
 
 let squares = document.getElementsByClassName("square");
 for (let i = 0; i < squares.length; i++) {
-	// squares[i].addEventListener("click", function() {
-	// 	console.log(`${i + 1} click!`);
-	// 	if (turn === "X") {
-	// 		squares[i].textContent = turn;
-	// 		turn = "O";
-	// 	} else {
-	// 		squares[i].textContent = turn;
-	// 		turn = "X";
-	// 	}
-	// });
 	squares[i].addEventListener("click", attempt);
 }
 
@@ -23,15 +17,48 @@ function attempt() {
 	console.log(`click!`);
 	if (turn === "X") {
 		this.textContent = turn;
-		turn = "O";
+		check();
 		this.removeEventListener("click", attempt);
+		turn = "O";
 	} else {
 		this.textContent = turn;
-		turn = "X";
+		check();
 		this.removeEventListener("click", attempt);
+		turn = "X";
 	}
 }
 
-function win() {
+function check() {
+	let tR = topRow.textContent.replace(/\s+/gi, "");
+	let mR = middleRow.textContent.replace(/\s+/gi, "");
+	let bR = bottomRow.textContent.replace(/\s+/gi, "");
 
+	console.log(tR);
+	console.log(mR);
+	console.log(bR);
+
+	if (tR === "XXX" || mR === "XXX" || bR === "XXX") {
+		win(turn);
+	} else if (tR === "OOO" || mR === "OOO" || bR === "OOO") {
+		win(turn);
+	} else if (false) {
+
+	} else if (false) {
+
+	} else if (false) {
+
+	} else if (false) {
+
+	} else {
+
+	}
+}
+
+
+
+function win(symbol) {
+	console.log(`${symbol}'s wins.`);
+	for (let i = 0; i < squares.length; i++) {
+		squares[i].removeEventListener("click", attempt);
+	}
 }
